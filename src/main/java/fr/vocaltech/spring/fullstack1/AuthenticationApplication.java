90e3e988b320fa6fdcbe586aa44d30d2d186896e
vocaltech <it@vocaltech.fr>
2022-03-21 23:20:04 +0100

    Remove exclude=SecurityAutoConfiguration from decorator

--- src/main/java/fr/vocaltech/spring/fullstack1/AuthenticationApplication.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/AuthenticationApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class AuthenticationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
-
 }
