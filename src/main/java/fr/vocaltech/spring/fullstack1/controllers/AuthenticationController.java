c03b46012f1c61c6d523b5baf5ea312832ac4b16
vocaltech <it@vocaltech.fr>
2022-03-21 23:21:32 +0100

    Replace path with /dashboard

--- src/main/java/fr/vocaltech/spring/fullstack1/controllers/AuthenticationController.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/controllers/AuthenticationController.java
@@ -5,18 +5,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
-
+//
 // TODO: Remove @CrossOrigin
+//
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/authentication")
+@RequestMapping("/dashboard")
 public class AuthenticationController {
   @GetMapping
   public Map<String, String> authentication() {
     Map<String, String> map = new HashMap<>();
-    map.put("message", "Greeting from AuthenticationController !");
+    map.put("id", UUID.randomUUID().toString());
+    map.put("message", "Welcome in your dashboard !");
 
     return map;
   }
