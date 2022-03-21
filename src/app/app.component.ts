6d18a7128aa8b629ee3df6052224397ae99b171b
vocaltech <it@vocaltech.fr>
2022-03-21 14:02:06 +0100

    add getting data from Spring server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment as env} from '../environments/environment'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ng-spring-security';
+  result!: any;
+
+  private authUrl = env.url_server + '/authentication';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.http.get(this.authUrl).subscribe(data => {
+        this.result = data;
+    })
+  }
 }
