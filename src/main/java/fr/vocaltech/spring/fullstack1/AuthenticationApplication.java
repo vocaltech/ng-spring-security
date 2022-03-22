17c33bdd490b8fd6b5c199fd55804ad6f2343b43
vocaltech <it@vocaltech.fr>
2022-03-22 22:19:38 +0100

    Cleanup imports

--- src/main/java/fr/vocaltech/spring/fullstack1/AuthenticationApplication.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/AuthenticationApplication.java
@@ -2,7 +2,6 @@ package fr.vocaltech.spring.fullstack1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class AuthenticationApplication {
