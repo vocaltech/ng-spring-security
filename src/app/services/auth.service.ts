a319b9d319c9a2a45af67482484ef2cbc6698119
vocaltech <it@vocaltech.fr>
2022-03-22 23:44:24 +0100

    implement login() function

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment as env} from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
 
-  constructor() { }
+export class AuthService {
+  private urlDashboard = env.url_server + '/dashboard';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  login = (username: string, password: string): Observable<Object> => {
+    const base64Encoded = btoa(`${username}:${password}`)
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + base64Encoded})
+
+    return this.http.get(this.urlDashboard, { headers })
+  }
 }
