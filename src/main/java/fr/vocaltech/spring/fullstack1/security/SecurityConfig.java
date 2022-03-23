30cce6c2ac7f52a8fe2a3d9a1c332cde65b698bf
vocaltech <it@vocaltech.fr>
2022-03-23 23:48:36 +0100

    Initial commit

---
+++ src/main/java/fr/vocaltech/spring/fullstack1/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package fr.vocaltech.spring.fullstack1.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  private final static String ORIGINS = "http://localhost:4200";
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+      // by default uses a Bean by the name of corsConfigurationSource
+    http
+      .cors(Customizer.withDefaults())
+      .authorizeRequests()
+      .anyRequest()
+      .authenticated()
+      .and()
+      .formLogin(Customizer.withDefaults())
+      .httpBasic(Customizer.withDefaults());
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Collections.singletonList(ORIGINS));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
