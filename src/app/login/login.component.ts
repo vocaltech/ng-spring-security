41151989d6c39987e7b3bed5c9b5badd00ad6710
vocaltech <it@vocaltech.fr>
2022-03-26 19:16:46 +0100

    initial implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  isLoading = false
 
-  constructor() { }
+  // This will give us access to the form
+  @ViewChild("loginform", { static: false }) 
+  loginForm!: NgForm;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) { }
+
+  ngOnInit(): void {}
+
+  onLoginSubmit = async () => {
+    const _username = this.loginForm.value.username;
+    const _password = this.loginForm.value.password;
+
+    // -------------------------
+    // --- update UI ---
+    // -------------------------
+    this.isLoading = true
+
+    // simulate a delay when sending data to server
+    await this.delay(2000);
+
+    this.isLoading = false
+
+    // -------------------------
+    // --- post data to server ---
+    // -------------------------
+    this.auth.login(_username, _password).subscribe(
+      data => {
+        console.log(data)
+        this.router.navigate(['dashboard'])
+      }),
+
+      (errors: any) => {
+        console.log(`Invalid credentials (username/password) : ${errors}`)
+      },
+
+      () => {}
   }
 
+  delay = (delayMs: number): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, delayMs)
+    })
+  }
 }
