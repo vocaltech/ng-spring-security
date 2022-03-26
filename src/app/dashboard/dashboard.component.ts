fd7107317b80e5c4b0f735bd0347b3c51542579c
vocaltech <it@vocaltech.fr>
2022-03-26 19:12:35 +0100

    Add isLoggedIn property

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  isLoggedIn = false;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
