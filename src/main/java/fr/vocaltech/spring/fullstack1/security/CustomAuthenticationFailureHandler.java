2ac6fa2edef5e445787c02c90df51d9a007428e5
vocaltech <it@vocaltech.fr>
2022-03-28 21:38:07 +0200

    Refactor outputStream with Map

--- src/main/java/fr/vocaltech/spring/fullstack1/security/CustomAuthenticationFailureHandler.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/security/CustomAuthenticationFailureHandler.java
@@ -1,22 +1,32 @@
 package fr.vocaltech.spring.fullstack1.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+  private ObjectMapper objectMapper = new ObjectMapper();
+
   @Override
-  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
     response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
-    String jsonPayload = "{\"message\" : \"%s\", \"code\" : \"%d\" }";
-    response.getOutputStream().println(String.format(jsonPayload, exception.getMessage(), response.getStatus()));
+    Map<String, String> map = new HashMap<>();
+    map.put("message", exception.getMessage());
+    map.put("status", String.valueOf(response.getStatus()));
+
+    System.out.println("[onAuthenticationFailure()] map: " + map);
+
+    response.getOutputStream().println(objectMapper.writeValueAsString(map));
   }
 }
