6b836b18cb4d5360212dcd487be6785c82f256c0
vocaltech <it@vocaltech.fr>
2022-04-01 20:44:23 +0200

    Implements UserDetails

--- src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserPrincipal.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserPrincipal.java
@@ -1,4 +1,44 @@
 package fr.vocaltech.spring.fullstack1.security;
 
-public class AuthUserPrincipal {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AuthUserPrincipal implements UserDetails {
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
 }
