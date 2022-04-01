6baf81d71f4c54da30ea7f38d08a86159e12b9fe
vocaltech <it@vocaltech.fr>
2022-04-01 20:43:44 +0200

    Implements UserDetailsService

--- src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserDetailsService.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserDetailsService.java
@@ -1,4 +1,26 @@
 package fr.vocaltech.spring.fullstack1.security;
 
-public class AuthUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthUserDetailsService implements UserDetailsService {
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    /*
+    User user = userRepository.findByUsername(username);
+
+    if (user == null)
+      throw new UsernameNotFoundException(username);
+
+    System.out.println("---> [CustomUserDetailsService.loadUserByUsername()] logged as: " + user.getUsername());
+    System.out.println("---> [CustomUserDetailsService.loadUserByUsername()] roles: " + user.getRoles());
+
+    return new CustomUserPrincipal(user);
+    */
+
+    return null;
+  }
 }
