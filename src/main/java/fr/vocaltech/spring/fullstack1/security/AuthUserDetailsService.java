93e5df6dad319ecd2a2b1c3ffe0d33563477bf34
vocaltech <it@vocaltech.fr>
2022-04-02 20:20:44 +0200

    Disable @Service

--- src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserDetailsService.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserDetailsService.java
@@ -3,9 +3,8 @@ package fr.vocaltech.spring.fullstack1.security;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class AuthUserDetailsService implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
