8fe73bad7f6333a63f08a84e9dd883b6585114a2
vocaltech <it@vocaltech.fr>
2022-04-02 20:28:09 +0200

    Add AuthGuard to dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: "login", pathMatch: 'full'},
   { path: "login", component: LoginComponent },
-  { path: "dashboard", component: DashboardComponent }
+  { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
