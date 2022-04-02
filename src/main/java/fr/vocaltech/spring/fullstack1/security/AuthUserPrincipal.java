3a38d8a556fb56b3e58d16994ed4d89b2f7bcaba
vocaltech <it@vocaltech.fr>
2022-04-02 20:22:00 +0200

    Implement UserDetails methods

--- src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserPrincipal.java
+++ src/main/java/fr/vocaltech/spring/fullstack1/security/AuthUserPrincipal.java
@@ -1,44 +1,67 @@
 package fr.vocaltech.spring.fullstack1.security;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import fr.vocaltech.spring.fullstack1.models.Role;
+import fr.vocaltech.spring.fullstack1.models.User;
 
 public class AuthUserPrincipal implements UserDetails {
+  private final User user;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+  public AuthUserPrincipal(User user) {
+    this.user = user;
   }
 
-  @Override
-  public String getPassword() {
-    return null;
+  public User getUser() {
+    return user;
   }
 
   @Override
   public String getUsername() {
-    return null;
+    return user.getUsername();
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    List<GrantedAuthority> authorities = new ArrayList<>();
+
+    Set<Role> roles = user.getRoles();
+    for (Role role: roles) {
+      authorities.add(new SimpleGrantedAuthority(role.getName().toString()));
+    }
+
+    return authorities;
   }
 
   @Override
   public boolean isAccountNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isCredentialsNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isEnabled() {
-    return false;
+    return true;
   }
 }
