677d483081086c6ddfbb46788d1d270397d4b81f
vocaltech <it@vocaltech.fr>
2022-04-03 13:14:28 +0200

    Add subscription to onLogin event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   isLoggedIn = false
+  username = ''
+  subscription!: Subscription;
 
-  constructor() {}
+  constructor(
+    private auth: AuthService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.auth.onLogin().subscribe({
+      next: v => {
+        this.isLoggedIn = v.isLoggedIn
+        this.username = v.username
+
+        this.cdr.detectChanges()
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 }
