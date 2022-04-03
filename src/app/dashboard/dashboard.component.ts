0702fc4cfa17ec6fe3bf62d37820e1020bbc0cb3
vocaltech <it@vocaltech.fr>
2022-04-03 13:16:48 +0200

    Add profile property

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
   isLoggedIn = false;
+  profile: any;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.profile = this.auth.getProfile()
+  }
 }
