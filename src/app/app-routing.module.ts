d38230d8a21d39bac75b2ff7a0f7dbd78da29c99
vocaltech <it@vocaltech.fr>
2022-04-03 13:12:41 +0200

    Add logout route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: "login", pathMatch: 'full'},
-  { path: "login", component: LoginComponent },
-  { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] }
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'logout', component: LogoutComponent }
 ];
 
 @NgModule({
